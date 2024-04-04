af5a4e9d4c829832bae48273b24bde18f02b7915
Sean Sovine <sean.r.sovine@gmail.com>
2024-04-03 22:19:31 -0400

    Add maybe_owning example

---
+++ README.md
@@ -0,0 +1,36 @@
+# C++ Examples
+
+This repo will contain small programs to try out different ideas in C++.
+
+## `maybe_owning`
+
+This has a class `PointerHolder` that acts as a wrapper for either a `std::unique_ptr` or
+a raw pointer. If it holds a raw pointer, then when the wrapper object is
+destroyed it deletes the pointer it holds. For a smart pointer the wrapper
+just holds the underlying pointer and assumes the smart pointer will handle
+deallocation whenever it is destroyed. So the name of the project is "maybe
+owning", since the wrapper class might or might not own the pointer it holds.
+
+The motivation for this is a situation where you have a `unique_ptr` variable
+that may need replaced in some local scope. Something like:
+
+```cpp
+auto ptr = std::make_unique< Thing >( args... );
+...
+
+{
+	PointerHolder ptrTwo;
+	if ( condition ) {
+		ptrTwo = PointerHolder( ptr );
+	} else {
+		ptrTwo = PointerHolder ( new Thing( args... ) );
+	}
+
+	func( *ptrTwo );
+	ptrTwo->memberFunc();
+}
+...
+
+funcTwo( *ptr );
+ptr->memberFunc();
+```
---
+++ maybe_owning/pointer_holder.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <memory>
+
+// Type trait idea from: https://stackoverflow.com/questions/65752626.
+
+template < typename T >
+struct is_unique_ptr : std::false_type {};
+template < typename T >
+struct is_unique_ptr< std::unique_ptr< T > > : std::true_type {};
+
+// Holds a raw pointer or unique pointer; if a raw pointer, deletes it on destruction.
+
+template < typename T, typename HeldType >
+class PointerHolder {
+  T* ptr;
+
+public:
+  template < typename U >
+  PointerHolder( const std::unique_ptr< U >& u ) : ptr{ u.get() } {};
+
+  template < typename U >
+  PointerHolder( U* u ) : ptr{ u } {};
+
+  template < typename U >
+  PointerHolder( std::unique_ptr< U >&& u ) : ptr{ u.release() } {};
+
+  template < typename U >
+  PointerHolder( U ) = delete;
+
+  ~PointerHolder() {
+    if constexpr ( !is_unique_ptr< HeldType >::value ) {
+      delete ptr;
+    }
+  };
+
+  T& operator*() {
+    return *ptr;
+  }
+
+  T* operator->() {
+    return ptr;
+  }
+};
+
+template < typename U >
+PointerHolder( const std::unique_ptr< U >& ) -> PointerHolder< U, std::unique_ptr< U > >;
+
+template < typename U >
+PointerHolder( std::unique_ptr< U >&& ) -> PointerHolder< U, U* >;
+
+template < typename U >
+PointerHolder( U* ) -> PointerHolder< U, U* >;
+
+/* Test it. */
+
+int main() {
+  auto stringPtr = std::make_unique< std::string >( "Hello!" );
+
+  PointerHolder holderOne( stringPtr );
+  PointerHolder holderTwo( new std::string( "Hello again!" ) );
+  PointerHolder holderTre( std::make_unique< std::string >( "And again!" ) );
+
+  std::cout << *holderOne << std::endl;
+  std::cout << *holderTwo << std::endl;
+  std::cout << *holderTre << std::endl;
+
+  std::cout << "Size of " << *holderOne << " is " << holderOne->size() << "." << std::endl;
+
+  return 0;
+};
